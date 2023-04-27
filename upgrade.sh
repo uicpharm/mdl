21b63c30b4bc05b69c6e14300fa094760ed11d5f
Josh Curtiss <josh@curtiss.me>
2023-04-27 12:03:43 -0500

    feat: Add upgrade script
    The script uses Git to fetch the latest source code changes, let the
    user select the branch to upgrade to, and it switches the code to that
    branch.
    
    Afterward, it attempts to reapply any diffs in the `customizations`
    directory.

---
+++ upgrade.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+scr_dir="$(realpath "${0%/*}")"
+envs_dir="$scr_dir/environments"
+mnames=$("$scr_dir/select-env.sh" "$1")
+
+for mname in $mnames; do
+
+   if [ ! -d "$envs_dir/$mname/src" ]; then
+      echo "Source code directory does not exist for $mname. Can't upgrade."
+      exit 1
+   fi
+
+   echo "Upgrading $mname..."
+   cd "$envs_dir/$mname/src" || exit 1
+
+   # Pull latest repo data
+   git remote remove nevoband 2>/dev/null
+   git remote set-url origin https://github.com/moodle/moodle.git
+   git fetch -np
+
+   # Remove any untracked code
+   git reset --hard
+   git clean -dfe local
+   curr_branch="$(git symbolic-ref --short HEAD)"
+   echo "Your current branch: $curr_branch"
+   git status -s -b
+
+   PS3="Select the version to upgrade to: "
+   select targetbranch in $(git branch -lr | grep -E "MOODLE_[34][0-9]+_STABLE" | cut -d"/" -f2); do
+      [ "$curr_branch" = "$targetbranch" ] && echo "You're on this branch now. Will fast forward to latest commit." || echo "Will switch to $targetbranch"
+      break
+   done
+
+   if [ "$curr_branch" = "$targetbranch" ]; then
+      git pull --ff-only --no-tags
+   else
+      git checkout -f --guess "$targetbranch"
+   fi
+   git apply -3 --whitespace=nowarn ../customizations/*.diff
+
+   cd "$scr_dir" || exit 1
+
+   echo 'Done!'
+
+done
