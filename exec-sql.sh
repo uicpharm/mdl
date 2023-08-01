180d6b8f254d46be7e49677abf4f1677ccd99a11
Josh Curtiss <josh@curtiss.me>
2023-08-01 09:33:30 -0500

    feat: Add `exec-sql` script for easily running SQL scripts in envs
    Similar to the `cli` script for PHP scripts, the `exec-sql` script will
    find the MariaDB container for an environment and execute either a SQL
    statement or a SQL file.
    
    If the received param is a file, it pipes in the file contents.
    Otherwise, it treats it like a SQL statement to execute.

---
+++ exec-sql.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+scr_dir="${0%/*}"
+envs_dir="$scr_dir/environments"
+mnames=$("$scr_dir"/select-env.sh "$1")
+
+for mname in $mnames; do
+
+   sql="$2"
+   sql_is_file=false
+   [ -f "$sql" ] && sql_is_file=true
+
+   env_dir="$envs_dir/$mname"
+   # shellcheck source=environments/sample.env
+   "$scr_dir/touch-env.sh" "$mname" && source "$env_dir/.env"
+
+   # Get an existing moodle task on this node
+   container="$(docker ps -q -f name="${mname}_mariadb" | head -1)"
+
+   if [ -n "$container" ]; then
+      if $sql_is_file; then
+         docker exec -i "$container" mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_NAME" "${@:3}" < "$sql"
+      else
+         docker exec -i "$container" mysql -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_NAME" "${@:3}" -e "$sql"
+      fi
+   else
+      echo "Could not find a container running MariaDB for Moodle for $mname!"
+      exit 1
+   fi
+
+done
