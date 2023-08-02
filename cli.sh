e2130c82169181b451a2c0ad69462d20fcf248d4
Josh Curtiss <josh@curtiss.me>
2023-08-02 08:41:03 -0500

    feat: Enhance CLI script to be ran programmatically
    - Does not run docker in interactive mode
    - If it cannot find a container, it will exit with a non-zero response
      to signal the error to the calling script.
    - Outputs just the output of the called command so it can be used
      programmatically to store or act on the command's output.

--- cli.sh
+++ cli.sh
@@ -16,10 +16,10 @@ for mname in $mnames; do
    container="$(docker ps -q -f name="${mname}_moodle" | head -1)"
 
    if [ -n "$container" ]; then
-      echo "Executing $cmd in $mname container $container..."
-      docker exec -it "$container" php "/bitnami/moodle/admin/cli/$cmd.php" "${@:3}"
+      docker exec -t "$container" php "/bitnami/moodle/admin/cli/$cmd.php" "${@:3}"
    else
       echo "Could not find a container running Moodle for $mname!"
+      exit 1
    fi
 
 done
