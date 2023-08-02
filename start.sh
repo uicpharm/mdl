3e7f6b5f70b3ec34c6f4a0c34d7495ff441edae4
Josh Curtiss <josh@curtiss.me>
2023-08-02 09:02:02 -0500

    feat: Enhance start script to have a `--wait` parameter
    If the `--wait` parameter is used, the start script will not exit until
    the Moodle environment is fully started. This means not just that the
    containers exist, but the Moodle "check" CLI task is returning success.
    
    This can be used by any scripts that need to wait until the Moodle
    environment is fully running before proceeding with other tasks.

--- start.sh
+++ start.sh
@@ -3,6 +3,14 @@
 scr_dir="${0%/*}"
 envs_dir="$scr_dir/environments"
 mnames=$("$scr_dir/select-env.sh" "$1")
+wait=false
+
+for arg in "$@"; do
+   if [ "$arg" == "--wait" ]; then
+      wait=true
+      break
+   fi
+done
 
 for mname in $mnames; do
 
@@ -14,6 +22,20 @@ for mname in $mnames; do
    # Use `--with-registry-auth` to ensure Docker uses authentication to our ghcr.io repo.
    # Use `--resolve-image never` for deploy to work on arm64 computers. Production VMs are amd64, and Macs can run amd64 in emulation.
    # Get configs from `docker compose config` so we can pull in environment file settings.
-   docker stack deploy --with-registry-auth --resolve-image never -c <(docker compose -f "$env_dir/docker-compose-$ver.yml" config | sed '/^name/d' | sed '/bind:/d' | sed '/create_host_path:/d' | sed -e '/published/ s/"//g') "$mname" "${@:2}"
+   docker stack deploy --with-registry-auth --resolve-image never -c <(docker compose -f "$env_dir/docker-compose-$ver.yml" config | sed '/^name/d' | sed '/bind:/d' | sed '/create_host_path:/d' | sed -e '/published/ s/"//g') "$mname"
+
+   if [ "$wait" == true ]; then
+      # Do not exit until environment is fully running
+      echo -n "Waiting for $mname to fully start up"
+      moodle_check=''
+      while [ -z "$moodle_check" ]; do
+         echo -n '.'
+         if ! moodle_check="$("$scr_dir/cli.sh" "$mname" checks --filter=core)"; then
+            moodle_check=''
+         fi
+         [ -z "$moodle_check" ] && sleep 6
+      done
+      echo ' Done!'
+   fi
 
 done
