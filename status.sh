f9378a7bf1a9c0db39c413a0c484a1d30c54cdc7
Josh Curtiss <josh@curtiss.me>
2023-08-02 10:20:28 -0500

    feat: Enhance status script to exit with error status if env not running
    By exiting with a non-zero error status if the requested environment is
    not running, we can use the status script programmatically to determine
    if an environment is running or not.

--- status.sh
+++ status.sh
@@ -5,6 +5,7 @@ mnames=$("$scr_dir/select-env.sh" "${1:-all}")
 norm="$(tput sgr0)"
 ul="$(tput smul)"
 bold="$(tput bold)"
+ok=true
 
 for mname in $mnames; do
 
@@ -14,6 +15,10 @@ for mname in $mnames; do
       echo
    else
       echo "The ${bold}$mname${norm} environment is ${ul}not running${norm}."
+      ok=false
    fi
 
 done
+
+# If an environment was not running, exit as an error
+[ "$ok" == true ] && exit 0 || exit 1
