09d01d717771e51d13613a81249fca2f9d27d279
Josh Curtiss <josh@curtiss.me>
2023-09-20 09:40:05 -0500

    fix: Adjust Moodle version calculation to be more resilient
    In the event that an unexpected branch is received that does not have a
    version number as expected, default to zero.
    
    Then, change `calc-docker-compose-version.sh` to use `moodle-version.sh`
    as expected. It will then always receive a number as expected.

--- calc-docker-compose-version.sh
+++ calc-docker-compose-version.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 scr_dir="${0%/*}"
-envs_dir="$scr_dir/environments"
 mname=$("$scr_dir"/select-env.sh "$1" --no-all)
-branchver="0"
-if [ -d "$envs_dir/$mname/src" ]; then
-   cd "$envs_dir/$mname/src" || exit 1
-   branchver="$(git symbolic-ref --short HEAD | cut -d"_" -f2)"
-fi
-[[ -z "$branchver" || "$branchver" -lt "401" ]] && echo "3.9.2" || echo "4.1.2"
+branchver=$("$scr_dir/moodle-version.sh" "$mname")
+[[ "$branchver" -lt "401" ]] && echo "3.9.2" || echo "4.1.2"
--- moodle-version.sh
+++ moodle-version.sh
@@ -8,4 +8,6 @@ if [ -d "$envs_dir/$mname/src" ]; then
    cd "$envs_dir/$mname/src" || exit 1
    branchver="$(git symbolic-ref --short HEAD | cut -d"_" -f2)"
 fi
+# If not numeric, set to zero.
+[[ "$branchver" =~ ^[0-9]+$ ]] || branchver="0"
 echo "$branchver"
