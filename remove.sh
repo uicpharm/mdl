3d67eaa8559467fab4da7fd8efe535edca921804
Josh Curtiss <josh@curtiss.me>
2024-08-27 15:14:21 -0500

    feat: Improve `mdl rm` command to remove env backup files if specified
    If label(s) for an environment backup are provided as the second parameter, the remove
    command will remove the backup files instead of the environment itself. If no labels are
    provided, it will still delete the environment like previously.
    
    Also added `rm` alias for the `remove` command.

--- remove.sh
+++ remove.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
 scr_dir="${0%/*}"
-envs_dir="$scr_dir/environments"
+envs_dir=$(realpath "$scr_dir/environments")
+backup_dir=$(realpath "$scr_dir/backup")
+
+# Parameter #1: Environment
 mnames=$("$scr_dir/select-env.sh" "$1")
+[[ $1 == all || $1 == "$mnames" ]] && shift
+
+# Parameter #2: Label, multiple can be provided
+labels="$*"
 
 for mname in $mnames; do
 
-   echo "Removing data for $mname..."
-   rm -Rf "$envs_dir/$mname/data" "$envs_dir/$mname/src" "$envs_dir/$mname/backup.sql"
-   db_vol_name=$(docker volume ls -q --filter "label=com.docker.compose.project=$mname" | grep db)
-   [ -n "$db_vol_name" ] && echo "Clearing the database Docker volume... $(docker volume rm "$db_vol_name")"
-
+   if [ -n "$labels" ]; then
+      # Remove a backup by environment/label
+      for label in $labels; do
+         echo "Removing backup for $mname with label $label..."
+         for file_type in src data db dbfiles; do
+            rm -fv "$backup_dir/${mname}_${label}_${file_type}".*
+         done
+      done
+   else
+      # Remove a moodle environment
+      echo "Removing data for $mname environment..."
+      rm -Rf "$envs_dir/$mname/data" "$envs_dir/$mname/src" "$envs_dir/$mname/backup.sql"
+      db_vol_name=$(docker volume ls -q --filter "label=com.docker.compose.project=$mname" | grep db)
+      [ -n "$db_vol_name" ] && echo "Clearing the database Docker volume... $(docker volume rm "$db_vol_name")"
+   fi
 done
