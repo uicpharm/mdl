e16a48bf72934a615a3b889e746f22184179bb94
Josh Curtiss <josh@curtiss.me>
2024-09-27 10:58:44 -0500

    feat: Enhance `mdl cli` to list available commands if no cmd is provided
    If you don't provide a command, `mdl cli` will query the environment for the list of
    commands in the cli directory.
    
    Also reworked the logic of checking that the container is running, since the command list
    also requires the container to be running to function.
    
    Lastly, fixed a "bug" where the '-h' or '--help' flag would display the `mdl cli` help
    screen no matter what, even if you tried to pass the flag to a CLI command.

--- cli.sh
+++ cli.sh
@@ -9,6 +9,9 @@ Usage: $(script_name) <ENV> <COMMAND> [ARGS...]
 Executes a Moodle CLI command in the environment you specify. You don't have to provide
 the .php extension of the script you want to run.
 
+If you don't provide the command you want to run, it will list all of the available
+commands provided by the environment.
+
 Options:
 -h, --help         Show this help message and exit.
 -i, --interactive  Enable interactive mode while executing the command.
@@ -26,10 +29,15 @@ Reset password:
 
 Purge cache:
    $bold$(script_name) \$mname purge_caches$norm
+
+Get list of available commands:
+   $bold$(script_name) \$mname$norm
 EOF
 }
 
-[[ $* =~ -h || $* =~ --help ]] && display_help && exit
+# Since they may use "-h" to get help in the CLI call, we only output our help screen if
+# they ONLY passed "-h" or "--help", and no other parameters.
+[[ $* == -h || $* == --help ]] && display_help && exit
 
 paramI=''
 for arg in "$@"; do
@@ -43,21 +51,19 @@ mnames=$("$scr_dir"/select-env.sh "$1")
 
 for mname in $mnames; do
 
-   cmd="$2"
-
-   if [ "$cmd" = "" ]; then
-      echo -n "Command to run: "
-      read -r cmd
-   fi
-
-   # Get an existing moodle task on this node
+   # Get the Moodle container for this environment
    container="$(docker ps -f "label=com.docker.compose.project=$mname" --format '{{.Names}}' | grep moodle | head -1)"
+   [[ -z $container ]] && echo "${red}Could not find a container running Moodle for $ul$mname$rmul!$norm" >&2 && exit 1
 
-   if [ -n "$container" ]; then
-      docker exec $paramI -t "$container" php "/bitnami/moodle/admin/cli/$cmd.php" "${@:3}"
-   else
-      echo "Could not find a container running Moodle for $mname!"
-      exit 1
+   cmd="$2"
+   # If they did not provide a cmd, list the available commands
+   if [[ -z $cmd ]]; then
+      echo "${bold}${ul}Available Commands$norm"
+      docker exec -t "$container" find /bitnami/moodle/admin/cli -maxdepth 1 -type f -exec basename {} .php \; | sort | sed 's/^/  - /'
+      exit
    fi
+
+   # Run the command, passing any additional arguments they passed on to this script
+   docker exec $paramI -t "$container" php "/bitnami/moodle/admin/cli/$cmd.php" "${@:3}"
 
 done
