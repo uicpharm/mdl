07133553410a0a2f2b6371fd1500cd7c9fa1865a
Josh Curtiss <josh@curtiss.me>
2024-10-14 10:25:44 -0500

    feat: Add existence check of backup.sql to `mdl status`

--- status.sh
+++ status.sh
@@ -24,6 +24,7 @@ for mname in $mnames; do
    running="$(docker ps -q -f name="$mname")"
    data_dir="$envs_dir/$mname/data"
    src_dir="$envs_dir/$mname/src"
+   sql_path="$envs_dir/$mname/backup.sql"
    db_vol_name=$(docker volume ls -q --filter "label=com.docker.compose.project=$mname" | grep db)
    docker_compose_path=$("$scr_dir/calc-docker-compose-path.sh" "$mname")
 
@@ -38,12 +39,14 @@ for mname in $mnames; do
    # Path info
    [ -d "$data_dir" ] && data_status="${green}exists" || data_status="${red}missing"
    [ -d "$src_dir" ] && src_status="${green}exists" || src_status="${red}missing"
+   [ -f "$sql_path" ] && sql_status="${green}exists" || sql_status="${red}missing"
    [ -n "$db_vol_name" ] && db_status="${green}exists" || db_status="${red}missing"
    # If db volume was not found, set the name to what it should've been
    [ -z "$db_vol_name" ] && db_vol_name="${mname}_db"
    echo "Paths:"
    echo "  - $data_dir ($data_status$norm)"
    echo "  - $src_dir ($src_status$norm)"
+   echo "  - $sql_path ($sql_status$norm)"
    echo "  - $db_vol_name ($db_status$norm)"
    # List Backups
    "$scr_dir/ls.sh" "$mname"
