9048a4caa734ec06aaf4faad8a83e510be48dc1e
Josh Curtiss <josh@curtiss.me>
2024-10-16 13:23:55 -0500

    feat: Add quiet mode to `mdl status`

--- status.sh
+++ status.sh
@@ -11,10 +11,12 @@ service names and IDs and data paths? What backup sets do they have?
 
 Options:
 -h, --help         Show this help message and exit.
+-q, --quiet        Quiet mode. Suppress normal output.
 EOF
 }
 
 [[ $* =~ -h || $* =~ --help ]] && display_help && exit
+[[ $* =~ -q || $* =~ --quiet ]] && quiet=true || quiet=false
 
 mnames=$("$scr_dir/select-env.sh" "${1:-all}")
 ok=true
@@ -28,35 +30,37 @@ for mname in $mnames; do
    db_vol_name=$(docker volume ls -q --filter "label=com.docker.compose.project=$mname" | grep db)
    docker_compose_path=$("$scr_dir/calc-docker-compose-path.sh" "$mname")
 
-   echo "${ul}Environment: $bold$mname$norm"
+   $quiet || echo "${ul}Environment: $bold$mname$norm"
    # Status
    if [ -n "$running" ]; then
-      echo "Status: ${green}running${norm}"
+      $quiet || echo "Status: ${green}running${norm}"
    else
-      echo "Status: ${red}not running${norm}"
+      $quiet || echo "Status: ${red}not running${norm}"
       ok=false
    fi
-   # Path info
-   [ -d "$data_dir" ] && data_status="${green}exists" || data_status="${red}missing"
-   [ -d "$src_dir" ] && src_status="${green}exists" || src_status="${red}missing"
-   [ -f "$sql_path" ] && sql_status="${green}exists" || sql_status="${red}missing"
-   [ -n "$db_vol_name" ] && db_status="${green}exists" || db_status="${red}missing"
-   # If db volume was not found, set the name to what it should've been
-   [ -z "$db_vol_name" ] && db_vol_name="${mname}_db"
-   echo "Paths:"
-   echo "  - $data_dir ($data_status$norm)"
-   echo "  - $src_dir ($src_status$norm)"
-   echo "  - $sql_path ($sql_status$norm)"
-   echo "  - $db_vol_name ($db_status$norm)"
-   # List Backups
-   "$scr_dir/ls.sh" "$mname"
-   # If running, the services list
-   if [ -n "$running" ]; then
+   $quiet || (
+      # Path info
+      [ -d "$data_dir" ] && data_status="${green}exists" || data_status="${red}missing"
+      [ -d "$src_dir" ] && src_status="${green}exists" || src_status="${red}missing"
+      [ -f "$sql_path" ] && sql_status="${green}exists" || sql_status="${red}missing"
+      [ -n "$db_vol_name" ] && db_status="${green}exists" || db_status="${red}missing"
+      # If db volume was not found, set the name to what it should've been
+      [ -z "$db_vol_name" ] && db_vol_name="${mname}_db"
+      echo "Paths:"
+      echo "  - $data_dir ($data_status$norm)"
+      echo "  - $src_dir ($src_status$norm)"
+      echo "  - $sql_path ($sql_status$norm)"
+      echo "  - $db_vol_name ($db_status$norm)"
+      # List Backups
+      "$scr_dir/ls.sh" "$mname"
+      # If running, the services list
+      if [ -n "$running" ]; then
+         echo
+         . "$scr_dir/export-env.sh" "$mname"
+         (cd "$scr_dir" && docker-compose -f "$docker_compose_path" ps 2>/dev/null)
+      fi
       echo
-      . "$scr_dir/export-env.sh" "$mname"
-      (cd "$scr_dir" && docker-compose -f "$docker_compose_path" ps 2>/dev/null)
-   fi
-   echo
+   )
 
 done
 
