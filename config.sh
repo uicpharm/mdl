4bc56786a9e7ce9aeed54757b8ea0ab64326bd71
Josh Curtiss <josh@curtiss.me>
2024-10-29 17:09:47 -0500

    feat: Add `mdl config` script to simplify Moodle configurations
    The `mdl config` script is partly a shortcut to use the `mdl cli` script to run Moodle's
    `cfg` admin CLI, similar to how `mdl resetpassword` is for the `reset_password` admin CLI.
    
    However, it also will scan the .env file and apply all valid Moodle configurations that
    are defined in it. Since Moodle configurations are always lowercase, there will never be
    any overlap with other configurations.
    
    By querying the `cfg` CLI for each key, it verifies that it will only attempt to set keys
    that are valid configs.

---
+++ config.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+. "${0%/*}/util/common.sh"
+
+display_help() {
+   cat <<EOF
+Usage: $(script_name) <ENV> [key] [value]
+
+Will issue a config get or set command for the Moodle environment. If you provide a key
+but no value, it will ${ul}get$norm the value. If you provide the value, it will ${ul}set$norm the value.
+
+If you do not provide a key/value pair, it will scan your environment file for valid keys
+and set them. This is a convenient way to set all the keys in your environment file.
+
+Since this command uses Moodle APIs, the environment must be started. If it isn't, the
+command will exit.
+
+Options:
+-h, --help         Show this help message and exit.
+EOF
+}
+
+[[ $* =~ -h || $* =~ --help ]] && display_help && exit
+
+mnames=$("$scr_dir/select-env.sh" "$1")
+key_param=$2
+val_param=$3
+[[ -n $key_param && -z $val_param ]] && get_mode=true || get_mode=false
+cli="$scr_dir/cli.sh"
+
+for mname in $mnames; do
+   $get_mode || echo "$bold${ul}Setting configurations for $mname$norm"
+   # The environment must be started. If not, abort.
+   ! "$scr_dir/status.sh" "$mname" -q && echo "The $mname environment is not started!" >&2 && continue
+   # If they provided a key, use that. Otherwise, use the contents of .env file.
+   if [[ -z $key_param ]]; then
+      env_file="$envs_dir/$mname/.env"
+      [[ ! -f $env_file ]] && echo "The $mname .env file does not exist!" >&2 && exit 1
+      env_content=$(cat "$env_file")
+   else
+      env_content="$key_param=$val_param"
+   fi
+   while IFS='=' read -r key value; do
+      # Skip lines that are either empty or commented out (start with '#')
+      if [ -z "$key" ] || [[ "$key" =~ ^[[:space:]]*# ]]; then
+         continue
+      fi
+      # Skip keys that are not lowercase (Moodle configs are always lowercase)
+      if [[ $key != $(echo "$key" | tr '[:upper:]' '[:lower:]') ]]; then
+         continue
+      fi
+      # Only set it if cfg.php says it is a valid configuration key
+      get_value=$($cli "$mname" cfg --name="$key" 2>/dev/null) && key_exists=true || key_exists=false
+      if $key_exists && ! $get_mode; then
+         echo "$bold$key:$norm $value" >&2
+         $cli "$mname" cfg --name="$key" --set="$value"
+      elif $key_exists; then
+         echo "$get_value"
+      else
+         echo "Skipped $key"
+      fi
+   done <<< "$env_content"
+done
