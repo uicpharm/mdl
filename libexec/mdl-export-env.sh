b45600a8a6c15e175a2dc89b5793f6a237d41d1e
Josh Curtiss <josh@curtiss.me>
2025-07-02 17:09:35 -0500

    feat: Improve `mdl export-env` to be more versatile and easy to read
    - Added support for (-u, --unset) flag that will UNSET variables of a .env file, rather
      than set them. This is useful for undoing the previous setting of variables from a .env
      file, even if there are custom values in it.
    - Added support for (--no-update-config) flag that will tell it to skip the process of
      updating the config.php file after reading in the values. Useful for scripts that know
      they won't need this work done, and thus will speed things up.
    - Improved to unset all standard config values itself, without needing to rely on a
      separate env file like blank.env to accomplish it.
    - Added the update config logic directly into this script so there no longer needs to be a
      mdl-update-config.sh script.
    - Added a more versatile `replace_config_value` function that can more adeptly handle the
      replacement of values from config.php because it will handle string or non-string
      values, although it can't handle something like arrays.
    - Added support to look for a `custom-config.sh` script in an environment directory, and
      if present, it will pass configs to that script where further customization can be done.
      Even better, it receives all existing functions and values, so it can use the function
      just mentioned, `replace_config_values`, to make the custom script very simple for
      customizations. And if you need something more complex, you are free to do so.

--- libexec/mdl-export-env.sh
+++ libexec/mdl-export-env.sh
@@ -2,22 +2,52 @@
 
 . "${0%/*}/../lib/mdl-common.sh"
 
+# Defaults
+update_config=true
+unset_mode=false
+
+# Help
 display_help() {
    cat <<EOF
 Usage: $(script_name) <ENV>
 
-Handles all the tasks of loading values from $ul.env$rmul so they are accessible to other
-scripts, including doing the initial touch of the .env file, updating configs, clearing
-out existing values, and finally, loading the new values.
+Handles all the tasks of setting or unsetting values from $ul.env$rmul so they can be used
+by other scripts. For thoroughness, this includes doing the initial touch of the .env
+file, clearing out existing values, loading the new values, and updating the environment's
+${ul}config.php$rmul with the corresponding values.
 
 Options:
--h, --help      Show this help message and exit.
+-h, --help               Show this help message and exit.
+-u, --unset              Unset all values in .env, instead of setting them.
+    --no-update-config   Don't update the environment's config.php with new values.
 EOF
 }
 
-[[ $* =~ -h || $* =~ --help ]] && display_help && exit
+# Positional parameter #1: Environment
+if [[ $1 == -* || -z $1 ]]; then
+   [[ $1 == -h || $1 == --help ]] || echo -e "${red}You MUST provide the environment.$norm\n" >&2
+   display_help; exit 1;
+else
+   mname=$("$scr_dir/mdl-select-env.sh" "$1" --no-all)
+   shift
+fi
 
-mname=$("$scr_dir/mdl-select-env.sh" "$1" --no-all)
+# Collect optional arguments.
+# shellcheck disable=SC2214
+# spellchecker: disable-next-line
+while getopts hu-: OPT; do
+   support_long_options
+   case "$OPT" in
+      h | help) display_help; exit 0 ;;
+      no-update-config) update_config=false ;;
+      u | unset) unset_mode=true ;;
+      \?) echo "${red}Invalid option: -$OPT$norm" >&2 ;;
+      *) echo "${red}Some of these options are invalid:$norm $*" >&2; exit 2 ;;
+   esac
+done
+shift $((OPTIND - 1))
+
+$unset_mode && update_config=false
 env_dir="$MDL_ENVS_DIR/$mname"
 
 export mname
@@ -25,13 +55,69 @@ export mname
 # We touch the .env file before we look at it.
 "$scr_dir/mdl-touch-env.sh" "$mname"
 
-# We update configs every time just so updating .env will naturally update the environment as well.
-"$scr_dir/mdl-update-config.sh" "$mname"
+# Clear all known vars to proactively avoid data leaks.
+variables=(
+   ROOT_PASSWORD DB_NAME DB_USERNAME DB_PASSWORD MOODLE_HOST WWWROOT
+   SOURCE_HOST SOURCE_DATA_PATH SOURCE_SRC_PATH SOURCE_DB_NAME SOURCE_DB_USERNAME SOURCE_DB_PASSWORD
+   BOX_CLIENT_ID BOX_CLIENT_SECRET BOX_REDIRECT_URI BOX_FOLDER_ID
+)
+for var in "${variables[@]}"; do
+   unset "$var"
+done
 
-# Clear the settings with blank.env to avoid any data leaks. Only look at UPPERCASE keys.
+# Set or unset this env data. Only look at UPPERCASE keys.
 # shellcheck disable=SC2046
-export $(grep -E '^[A-Z_0-9]+=' "$MDL_ENVS_DIR/blank.env" | xargs)
+if $unset_mode; then
+   unset $(grep -E '^[A-Z_0-9]+=' "$env_dir/.env" | cut -d= -f1 | xargs)
+else
+   export $(grep -E '^[A-Z_0-9]+=' "$env_dir/.env" | xargs)
+fi
 
-# Load this env data. Only look at UPPERCASE keys.
-# shellcheck disable=SC2046
-export $(grep -E '^[A-Z_0-9]+=' "$env_dir/.env" | xargs)
+# Update configs in environment's config.php file.
+if $update_config; then
+   env_config_file="$env_dir/src/config.php"
+
+   # Get desired wwwroot value
+   if [ -z "$WWWROOT" ]; then
+      defaultwwwroot="$(grep -o -E "CFG->wwwroot\s*=\s*'(.*)';" "$env_config_file" | cut -d"'" -f2)"
+      altwwwroot1="http://$HOSTNAME"
+      [ "$defaultwwwroot" = "$altwwwroot1" ] && altwwwroot1=''
+      altwwwroot2="http://$MOODLE_HOST"
+      [ "$defaultwwwroot" = "$altwwwroot2" ] && altwwwroot2=''
+      echo 'You can avoid this prompt by setting WWWROOT in your .env file.'
+      PS3="Select a desired wwwroot value or type your own: "
+      select WWWROOT in "$defaultwwwroot" $altwwwroot1 $altwwwroot2; do
+         WWWROOT="${WWWROOT:-$REPLY}"
+         break
+      done
+   fi
+
+   replace_config_value() {
+      local content="$1"
+      [[ -t 0 ]] && shift || content="$(cat)"
+      local key="$1"
+      local val="$2"
+      echo "$content" | sed -E "s|(\\\$CFG->${key}[[:space:]]*=[[:space:]]*'?)[^';]*('?;)|\1${val}\2|g"
+   }
+
+   # Replace values in config
+   if [[ -f "$env_config_file" ]]; then
+      # Standard updates
+      config_content=$(
+         sed -E "/'dbport'/ s/> .*,/> 3306,/" "$env_config_file" |
+         replace_config_value dbtype "${DB_TYPE:-mariadb}" |
+         replace_config_value dbhost "${DB_HOST:-mariadb}" |
+         replace_config_value dbname "$DB_NAME" |
+         replace_config_value dbuser "$DB_USERNAME" |
+         replace_config_value dbpass "$DB_PASSWORD" |
+         replace_config_value wwwroot "$WWWROOT" |
+         replace_config_value dataroot "${DATA_ROOT:-/bitnami/moodledata}"
+      )
+      # Run custom updates if provided
+      custom_script="$env_dir/custom-config.sh"
+      # shellcheck disable=SC1090
+      [[ -f $custom_script ]] && config_content=$(. "$custom_script" "$config_content")
+      # Write new contents to config file
+      [[ -n $config_content ]] && echo "$config_content" > "$env_config_file"
+   fi
+fi
