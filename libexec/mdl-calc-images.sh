5a4504f18d22a85711f0e2b600720503290bdae5
Josh Curtiss <josh@curtiss.me>
2025-07-03 12:11:33 -0500

    feat: Add support for downloadable version matrix

--- libexec/mdl-calc-images.sh
+++ libexec/mdl-calc-images.sh
@@ -5,19 +5,38 @@
 
 port_chart_settings="%-15s %-8s\n"
 ver_chart_settings="%-9s %-11s %-11s\n"
-versions=(
-#   Branch  Moodle   MariaDB
-   "39      3.9.2    10.5.27"
-   "310     3.10.4   10.5.27"
-   "311     3.11.12  10.5.37"
-   "400     4.0.11   10.6.20"
-   "401     4.1.15   10.11.11"
-   "402     4.2.11   10.11.11"
-   "403     4.3.8    10.11.11"
-   "404     4.4.4    10.11.11"
-   "405     4.5.4    10.11.11"
-   "500     5.0.0    10.11.11"
-)
+
+# Load version matrix, including assessing if we need to download from internet
+should_download_versions_file=false
+[[ $* =~ -u || $* =~ --update ]] && should_download_versions_file=true
+if [[ ! -f "$MDL_VERSIONS_FILE" ]]; then
+   echo "Versions file not found. Expected at: $MDL_VERSIONS_FILE" >&2
+   should_download_versions_file=true
+elif (( MDL_VERSIONS_SOURCE_CHECK_FREQUENCY > 0 )); then
+   mod=$(date -r "$MDL_VERSIONS_FILE" +%s 2>/dev/null || echo 0)
+   now=$(date +%s)
+   (( now - mod > MDL_VERSIONS_SOURCE_CHECK_FREQUENCY )) && should_download_versions_file=true
+fi
+if $should_download_versions_file; then
+   echo "Downloading versions file from: $MDL_VERSIONS_SOURCE_URL"
+   if new_versions_content=$(curl -fsL "$MDL_VERSIONS_SOURCE_URL"); then
+      echo "$new_versions_content" > "$MDL_VERSIONS_FILE"
+   else
+      echo "Failed! Please check your internet connection or the URL." >&2
+      exit 1
+   fi
+fi
+if [[ ! -f "$MDL_VERSIONS_FILE" ]]; then
+   echo 'Cannot get versions. Aborting.' >&2
+   exit 1
+fi
+
+versions=()
+versions_content=$(< "$MDL_VERSIONS_FILE")
+include_line=false
+while read -r line; do
+   $include_line && versions+=("$line") || include_line=true
+done <<< "$versions_content"
 first_ver=${versions[0]%% *}
 last_ver=${versions[$((${#versions[@]} - 1))]%% *}
 
@@ -52,12 +71,21 @@ $(
 
 Options:
 -h, --help      Show this help message and exit.
+-u, --update    Update versions file now.
 EOF
 }
 
 [[ $* =~ -h || $* =~ --help ]] && display_help && exit
 
-mname=$("$scr_dir"/mdl-select-env.sh "$1" --no-all)
+# Positional parameter #1: Environment
+if [[ $1 == -* || -z $1 ]]; then
+   # Allow no env to be specified, in which case we just stop here.
+   # This allows this script to at least do the version matrix download.
+   exit
+else
+   mname=$("$scr_dir/mdl-select-env.sh" "$1" --no-all)
+fi
+
 branchver=$("$scr_dir/mdl-moodle-version.sh" "$mname")
 
 # Handle defaults if an unexpected branch version is used
