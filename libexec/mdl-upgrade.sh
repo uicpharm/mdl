a235975c43ef4f1c84d807aa3dd8273a40a9e29b
Josh Curtiss <josh@curtiss.me>
2025-07-31 00:36:50 -0500

    feat: Improve `mdl upgrade` to more efficiently use git repo
    Rather than just expecting the entire Moodle repo to be fetched, the `mdl upgrade` script
    runs a little more efficiently now:
    
    - Uses git ls-remote to list available branches without fetching them
    - Fetches updates for only the branch we're interested in
    - If switching branches, delete the old branch and go git garbage collection
    
    Separately, it seems there are some circumstances where permissions are an issue after
    performing an upgrade. To be safe, we always set permissions at the end.

--- libexec/mdl-upgrade.sh
+++ libexec/mdl-upgrade.sh
@@ -22,7 +22,7 @@ EOF
 
 [[ $* =~ -h || $* =~ --help ]] && display_help && exit
 
-requires docker grep cut
+requires docker grep cut uuidgen
 
 mnames=$("$scr_dir/mdl-select-env.sh" "$1")
 targetbranch="$2"
@@ -40,25 +40,22 @@ for mname in $mnames; do
 
    echo "Upgrading $mname..."
    function git_cmd() {
-      docker run --rm --name "${mname}_worker_git" \
-         -v "$src_vol_name":/src -w /src "$MDL_GIT_IMAGE" \
-         -c safe.directory=/src "$@"
+      docker run --rm --name "$mname-git-$(uuidgen)" -v "$src_vol_name":/git "$MDL_GIT_IMAGE" -c safe.directory=/git "$@"
    }
 
    # Pull latest repo data
    git_cmd remote set-url origin https://github.com/moodle/moodle.git
-   git_cmd fetch -np
+   curr_branch="$(git_cmd symbolic-ref --short HEAD)"
+   targetbranches="$(git_cmd ls-remote --heads 2>/dev/null | grep -E "MOODLE_[3-9][0-9]+_STABLE" | cut -d"/" -f3)"
 
    # Remove any untracked code
    git_cmd stash save -u
    git_cmd reset --hard
    git_cmd clean -dfe local
-   curr_branch="$(git_cmd symbolic-ref --short HEAD)"
    echo "Your current branch: $curr_branch"
    git_cmd status -s -b
 
-   # Get list of branches. If targetbranch isn't in list, prompt user to select one
-   targetbranches="$(git_cmd branch -lr | grep -E "MOODLE_[3-9][0-9]+_STABLE" | cut -d"/" -f2)"
+   # If targetbranch isn't in branch list, prompt user to select one
    echo "$targetbranches" | grep -qw "$targetbranch" || targetbranch=""
    if  [ -z "$targetbranch" ]; then
       PS3="Select the version to upgrade to: "
@@ -67,18 +64,31 @@ for mname in $mnames; do
          echo "$targetbranches" | grep -qw "$targetbranch" && break
       done
    fi
-   [ "$curr_branch" = "$targetbranch" ] && echo "You're on this branch now. Will fast forward to latest commit." || echo "Will switch to $targetbranch"
 
    # Pull/checkout new code
    if [ "$curr_branch" = "$targetbranch" ]; then
-      git_cmd pull --ff-only --no-tags
+      echo "You're on this branch now. Will fast forward to latest commit."
+      git_cmd pull --ff-only --no-tags origin "$targetbranch"
    else
+      echo "Will switch to $ul$targetbranch$norm."
+      git_cmd fetch -np origin "$targetbranch"
       git_cmd checkout -f --guess "$targetbranch"
+      git_cmd branch -D "$curr_branch"
+      git_cmd branch -rd "origin/$curr_branch"
+      echo Performing git optimizations...
+      git_cmd gc --prune=now
    fi
 
    # Pop stash
    git_cmd stash pop
 
-   echo 'Done!'
+   # Fix permissions
+   docker run --rm --name "${mname}_worker_fix_perms" \
+      -v "$src_vol_name":/src "$MDL_SHELL_IMAGE" sh -c "\
+         chown -R daemon:daemon /src
+         chmod -R g+rwx /src
+      "
+
+   echo '🎉 Done!'
 
 done
